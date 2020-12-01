b896914db55a143cdce5ec52a7df5ce70a7b0926
omerfarukpolat <polatfaruk06@gmail.com>
2020-12-01 23:00:29 +0300

    search tag model added

---
+++ UygulamaOdevi2/Models/SEARCH_TAGS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UygulamaOdevi2.Models
+{
+    public class SEARCH_TAGS
+    {
+        public string confID;
+        public string confName;
+        public string tags;
+    }
+}
